fedb582bbb913a29165d0dd5bb647a5b8a7ce1c3
HeavyLaw <576010062@qq.com>
2022-03-01 21:03:28 +0800

    fix: Add padding operations, adapt features and labels of different shapes

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,7 +6,7 @@ class NaiveBayes:
     def __init__(self, features, label):
         self.features = features
         self.label = label
-        self.features_type = []
+        self.features_type: list = []
         for x in range(features.shape[1]):
             self.features_type.append(np.unique(features[:, x]).tolist())
         self.label_type = np.unique(label).tolist()
@@ -22,9 +22,7 @@ class NaiveBayes:
             prior_prob_list.append(alist.count(c) / len(alist))
         return np.array(prior_prob_list)
 
-
-    @staticmethod
-    def cal_conditional_probability(features, label):
+    def cal_conditional_probability(self, features, label):
         conditional_prob_list = []
         c_type = np.unique(label)
 
@@ -36,7 +34,10 @@ class NaiveBayes:
 
             # 计算每类各个属性对应的取值的条件概率矩阵 p(x|y)
             for x in range(features.shape[1]):
-                conditional_prob_list.append(NaiveBayes.cal_prior_probability(np.array(features_split)[:, x]))
+                x_prob = self.cal_prior_probability(np.array(features_split)[:, x])
+                padding = len(self.features_type[x]) - len(x_prob)
+                x_prob_padding = np.pad(x_prob, (0, padding))
+                conditional_prob_list.append(x_prob_padding)
         return np.array(conditional_prob_list)
 
     def train(self):
@@ -46,6 +47,7 @@ class NaiveBayes:
 
     def test(self, test_data):
         max_prob = 0
+        max_prob_label = None
         for i, c in enumerate(self.label_metric):
             sum_feature_prob = 1
             for j, x in enumerate(test_data):
@@ -60,11 +62,13 @@ class NaiveBayes:
 X_train = np.array([[1, 'S'], [1, 'M'], [1, 'M'], [1, 'S'], [1, 'S'], [2, 'S'],
            [2, 'M'], [2, 'M'], [2, 'L'], [2, 'L'], [3, 'L'], [3, 'M'], [3, 'M'],
            [3, 'L'], [3, 'L']])
-y_train = np.array([-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1])
+y_train = np.array([-1, -1, 1, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, -1])
 
 a = NaiveBayes(X_train, y_train)
 a.train()
 print(a.test([2, 'S']))
+
+
 
 
 
